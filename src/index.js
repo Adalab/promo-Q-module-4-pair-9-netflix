d561b4f857d8a53b0ff3c02c1cabf1d707f0c5a8
lupeMorales <morales.lupegmail.com>
2022-09-07 15:52:21 +0200

    filter by gender

--- src/index.js
+++ src/index.js
@@ -13,17 +13,25 @@ server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 //endpoint
-server.get("/movie/:movieId", (req, res) => {
-  console.log(req.params);
-});
 
 server.get("/movies", (req, resp) => {
+  const genderFilterParam = req.query.gender;
+  const moviesFilter = movies.filter((item) => {
+    if (item.gender === genderFilterParam) {
+      console.log("bien hecho, guapi");
+    }
+  });
   const responseSuccesse = {
     success: true,
     movies: movies,
   };
+  console.log(req.params);
   resp.json(responseSuccesse);
 });
+//consigue id de la peli a renderizar
 
+server.get("/movie/:movieId", (req, res) => {
+  console.log(req.params);
+});
 const staticServerPathWeb = "./src/public-react"; // En esta carpeta ponemos los ficheros estáticos
 server.use(express.static(staticServerPathWeb));
--- web/src/services/api-movies.js
+++ web/src/services/api-movies.js
@@ -1,7 +1,8 @@
 // login
 
-const getMoviesFromApi = () => {
+const getMoviesFromApi = (params) => {
   console.log("Se están pidiendo las películas de la app");
+  console.log(params);
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
   return fetch("http://localhost:4000/movies")
     .then((response) => response.json())
